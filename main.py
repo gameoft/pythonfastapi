4576c7eb007471a35491de03f70126dbfe3d4f19
riccardo <dellasantar@gmail.com>
2023-04-19 22:18:47 +0200

    Update main.py
    get and post

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-#first commit
+# main backend lAPI server file
+
+from fastapi import FastAPI
+from fastapi import Body
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello to my API"}
+
+
+@app.get("/posts")
+def get_posts():
+    return {"data": "This is your posts"}
+
+
+@app.post("/create")
+def create_posts(payload: dict = Body(...)):
+    print(payload)
+    return {"new_post": f"title {payload['title']} content {payload['content']}"}
